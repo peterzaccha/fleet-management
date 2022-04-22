d304a962fc3a50af131ef3eaa0a9ee8fc6179252
peterzaccha <p.pator@outlook.com>
2022-04-23 01:04:29 +0200

    add users to the nav bar

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -2,11 +2,8 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Cities') }}
+                {{ __('Bookings') }}
             </h2>
-            <a type="button"  href="{{route('cities.create')}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                Add
-            </a>
         </div>
     </x-slot>
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,6 +24,9 @@
                     <x-nav-link :href="route('bookings.index')" :active="request()->routeIs('bookings.index')">
                         {{ __('Bookings') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
+                        {{ __('Users') }}
+                    </x-nav-link>
                     <x-nav-link :href="route('apis.docs')" :active="request()->routeIs('apis.docs')">
                         {{ __('Api Documentaion') }}
                     </x-nav-link>
