9502ff4d407ad5890c1203def48c5303566d59e4
peterzaccha <p.pator@outlook.com>
2022-04-22 03:08:10 +0200

    adding empty seats indecator in the dashboard

--- resources/views/trips/cities.blade.php
+++ resources/views/trips/cities.blade.php
@@ -1,7 +1,12 @@
 
+<div class="flex space-x-1 max-w-md flex-wrap">
 @foreach ($row->cities as $city)
-    {{$city->name}}
+    <span class="{{$city->pivot->empty_seats > 8 ? "bg-green-300": ($city->pivot->empty_seats > 4 ? "bg-yellow-300":"bg-red-300")}} rounded-full px-2 py-2 mb-2">
+        {{$city->name}}
+        <span class="px-2 py-1 text-xs rounded-full bg-white ">{{$city->pivot->empty_seats}}</span>
+    </span>
     @unless ($loop->last)
         <svg class="inline w-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"/></svg>
     @endunless 
 @endforeach
+</div>
