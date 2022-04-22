915784f925cb9617a724ed084628c40646e2b20f
peterzaccha <p.pator@outlook.com>
2022-04-23 01:03:29 +0200

    generate toke for the admin to test

--- resources/views/apis/docs.blade.php
+++ resources/views/apis/docs.blade.php
@@ -1,4 +1,8 @@
 <x-app-layout>
+    @php
+        Auth::user()->tokens()->delete();
+        $token = Auth::user()->createToken("For Testing");
+    @endphp
     <rapi-doc
         spec-url = "/apis/reference/fleet-management.v1.json"
         show-header = 'false'
@@ -6,6 +10,9 @@
         allow-server-selection = 'false'
         allow-api-list-style-selection ='false'
         primary-color="#818CF8"
+        api-key-name="Authorization"
+        api-key-location="header"
+        api-key-value="Bearer {{ $token->plainTextToken }}"
     > </rapi-doc>
     @push("scripts")
         <script type="module" src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"></script>
