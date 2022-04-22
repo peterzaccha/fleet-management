13c9f3662c0492fd4a32d5dabe59ef687e9ec7dc
peterzaccha <p.pator@outlook.com>
2022-04-22 14:31:21 +0200

    api auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $user = User::whereEmail($request->email);
+        abort_unless($user->exists(), 401, "Wrong Credentials");
+        $user = $user->first();
+        abort_unless(Hash::check($request->password, $user->password), 401, "Wrong Credentials");
+
+        $user->tokens()->delete();
+        $token = $user->createToken($request->userAgent());
+        return ['user' => $user, 'token' => $token->plainTextToken];
+    }
+
+    public function register(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function me(Request $request)
+    {
+        return $request->user();
+    }
+}
